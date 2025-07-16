9ed8111c641e80a5f2e45d3bc0cd699773a091aa
Jatin Saini <jatinrohitsaini@gmail.com>
2025-07-16 18:03:04 +0530

    Update: recent changes

--- src/components/GptSearch.jsx
+++ src/components/GptSearch.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-// import GptSearchBar from './GptSearchBar'
+import GptSearchBar from './GptSearchBar'
 
 const GptSearch = () => {
   return (
     <div>
-      {/* <GptSearchBar/> */}
-      hello
+      <GptSearchBar/>
     </div>
   )
 }
