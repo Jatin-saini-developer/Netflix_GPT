0b20615646f93badb73105b2f6d2c8c356fcd066
Jatin Saini <jatinrohitsaini@gmail.com>
2025-08-08 23:45:48 +0530

    Changes in UI

--- src/components/GptSearchBar.jsx
+++ src/components/GptSearchBar.jsx
@@ -1,11 +1,15 @@
 import React, { useRef } from "react";
 import client from "../utils/OpenAi";
-import {OPEN_API} from "../utils/constants";
+import { OPEN_API } from "../utils/constants";
 
 const GptSearchBar = () => {
   const search = useRef(null);
 
   const handleGptSearchClick = async () => {
+    (e) => {
+      e.preventDefault();
+    };
+
     console.log(search.current.value);
 
     const gptQuery =
@@ -16,7 +20,7 @@ const GptSearchBar = () => {
     const completion = await client.chat.completions.create({
       // headers: {
       //   "Content-Type": "application/json",
-      //   Authorization: `Bearer ${OPEN_API} `, 
+      //   Authorization: `Bearer ${OPEN_API} `,
       // },
       model: "mistralai/mistral-7b-instruct",
       messages: [{ role: "user", content: gptQuery }],
@@ -27,17 +31,23 @@ const GptSearchBar = () => {
   };
 
   return (
-    <div className="pt-40 flex justify-center w-full max-w-7xl">
+    <div className="pt-35 flex  justify-center w-full max-w-7xl">
       <form
-    className="  px-4 flex gap-2"
+        className="w-[1280px] max-w-4xl px-4 flex gap-2 "
         onSubmit={(e) => {
           e.preventDefault();
         }}
       >
-        <input className="bg-white" ref={search} type="search" placeholder="What Would You Like to Watch..." />
+
+        <input
+          className="px-2 max-w-full bg-white flex flex-1 focus:outline-none focus:ring-0 focus:border-transparent "
+          ref={search}
+          type="search"
+          placeholder="What Would You Like to Watch..."
+        />
 
         <button
-          className="bg-violet-800 text-white px-4 py-2 rounded-lg"
+          className="bg-[#da2c38] text-white px-4 py-2 rounded-lg"
           onClick={handleGptSearchClick}
         >
           Search
